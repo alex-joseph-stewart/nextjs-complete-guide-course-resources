e82a3703c82fae96c5a0d52bc6d007bb12166c15
Alex <alexjstewart220@gmail.com>
2025-04-11 15:35:15 -0400

    feat: adds images

--- code/02-nextjs-essentials/05-onwards-foodies-starting-project/components/main-header/main-header.js
+++ code/02-nextjs-essentials/05-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -1,10 +1,14 @@
+
+
 import logoImg from '@/assets/logo.png';
 import Link from 'next/link';
 import classes from './main-header.module.css'
 import Image from 'next/image'
 import MainHeaderBackground from './main-header-background';
+import NavLink from './nav-link';
 
 export default function MainHeader(){
+
     return (
     <>
     <MainHeaderBackground />
@@ -17,8 +21,10 @@ export default function MainHeader(){
         <nav className={classes.nav}>
             <ul>
                 <li>
-                    <Link href="/meals">Browse Meals</Link>
-                    <Link href="/community">Foodies Community</Link>
+                    <NavLink href="/meals">Browse Meals</NavLink>
+                </li>
+                <li>
+                    <NavLink href="/community">Foodies Community</NavLink>
                 </li>
             </ul>
         </nav>
